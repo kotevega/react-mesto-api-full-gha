a5a55b52b78849ad6256fff107d30f0b05cf6f89
Konstantin Logunov <keniglokon@yandex.ru>
2023-08-10 18:43:43 +0200

    fix: test

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-const { PORT, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
